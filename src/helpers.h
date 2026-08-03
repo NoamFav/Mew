71c31e84348086ee71a9b0a7ab47bd9cb67cafb4
NoamFav <95364866+NoamFav@users.noreply.github.com>
2026-08-03 17:53:01 +0200

    feat(display_file): implement flushing and line numbering output

--- src/helpers.h
+++ src/helpers.h
@@ -3,6 +3,7 @@
 
 #define PROGNAME "mew"
 #define BUF_SIZE 4096
+#define NUM_WIDTH 6
 
 #include <errno.h>
 #include <string.h>
@@ -11,4 +12,7 @@ int getlen(const char *str);
 int file_error(const char *name);
 int write_all(int fd, const char *buf, ssize_t len);
 void usage_exit(const char *prog, int c);
+void *nf_memcpy(void *dst, const void *src, size_t n);
+size_t num_to_buf(size_t n, char *num);
+size_t count_digits(size_t n);
 #endif
