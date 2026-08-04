966f0c815c132320f91c6fb970d6997e7ef6981d
NoamFav <95364866+NoamFav@users.noreply.github.com>
2026-08-05 00:42:57 +0200

    feat(display_file, render): Support non-printing characters with unsigned char and extended speci...

--- src/out/render.h
+++ src/out/render.h
@@ -4,8 +4,8 @@
 #include "out/outbuf.h"
 #include "parser.h"
 
-int is_special(char c, t_opts opts);
-int emit_special(char c, t_opts opts, t_outbuf *ob);
+int is_special(unsigned char c, t_opts opts);
+int emit_special(unsigned char c, t_opts opts, t_outbuf *ob);
 int emit_prefix(t_linestate *ls, t_opts o, t_outbuf *ob, int blank);
 
 #endif
