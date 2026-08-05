7cedb61d5a610b0e9d98baf276e7ecd92c9a7cb0
NoamFav <95364866+NoamFav@users.noreply.github.com>
2026-08-05 16:03:37 +0200

    feat(mew): add version number and support for long options in mew and parser

--- includes/mew.h
+++ includes/mew.h
@@ -1,6 +1,8 @@
 #ifndef MEW_H
 #define MEW_H
 
+#define VERSION "v1.0.0"
+
 int mew(int argc, char **argv);
 
 #endif
--- src/util/error.h
+++ src/util/error.h
@@ -9,5 +9,8 @@
 
 int file_error(const char *name);
 void usage_exit(const char *prog, int c);
+void usage_exit_long(const char *prog, const char *s);
+void print_help(const char *prog);
+void print_version(const char *prog);
 
 #endif
